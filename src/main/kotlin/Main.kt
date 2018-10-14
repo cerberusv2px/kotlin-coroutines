9d500bf0656db3890cf9073731e324d9e4753e6c
Sujin <sujin.v2px@gmail.com>
2018-10-14 12:15:46 +0545

    coroutine scope

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main(args: Array<String>) {
     // exampleBlocking()
     //exampleBlockingAlternative()
-    exampleBlockingDispatcher()
+    //exampleBlockingDispatcher()
+    //exampleLaunchGlobalWaiting()
+    exampleLaunchCoroutineScope()
 }
 
 suspend fun printlnDelayed(message: String) {
@@ -35,4 +35,31 @@ fun exampleBlockingDispatcher() {
     // Outside of runBlocking to show that irs running in the blocked main thread
     println("three - from thread ${Thread.currentThread().name}")
     // it still runs only after the runBlocking is fully executed.
+}
+
+fun exampleLaunchGlobal() = runBlocking {
+    println("one - from thread ${Thread.currentThread().name}")
+    GlobalScope.launch {
+        printlnDelayed("two - from thread ${Thread.currentThread().name}")
+    }
+    println("three - from thread ${Thread.currentThread().name}")
+    delay(3000)
+}
+
+fun exampleLaunchGlobalWaiting() = runBlocking {
+    println("one - from thread ${Thread.currentThread().name}")
+    val job = GlobalScope.launch {
+        printlnDelayed("two - from thread ${Thread.currentThread().name}")
+    }
+    println("three - from thread ${Thread.currentThread().name}")
+    job.join()
+}
+
+fun exampleLaunchCoroutineScope() = runBlocking {
+    println("one - from thread ${Thread.currentThread().name}")
+    launch(Dispatchers.Default){
+        printlnDelayed("two - from thread ${Thread.currentThread().name}")
+    }
+    println("three - from thread ${Thread.currentThread().name}")
+
 }
